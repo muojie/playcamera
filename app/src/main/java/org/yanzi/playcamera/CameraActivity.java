8103d848038208343e3dc00f2706a47ddd3fd6da
guoxinjie <htc.guo@gmail.com>
2015-12-26 16:03:43 +0800

    copy from PlayCamera_V3.0.0 (eclipse)
    can run on my TCL phone

--- app/src/main/java/org/yanzi/playcamera/CameraActivity.java
+++ app/src/main/java/org/yanzi/playcamera/CameraActivity.java
@@ -1,37 +1,93 @@
 package org.yanzi.playcamera;
 
-import android.support.v7.app.AppCompatActivity;
+import org.yanzi.playcamera.CameraInterface;
+import org.yanzi.playcamera.CameraInterface.CamOpenOverCallback;
+import org.yanzi.playcamera.preview.CameraGLSurfaceView;
+import org.yanzi.playcamera.R;
+import org.yanzi.playcamera.util.DisplayUtil;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.TextureView.SurfaceTextureListener;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageButton;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends Activity {
+	private static final String TAG = "yanzi";
+	CameraGLSurfaceView glSurfaceView = null;
+	ImageButton shutterBtn;
+	float previewRate = -1f;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+		initUI();
+		initViewParams();
+		
+		shutterBtn.setOnClickListener(new BtnListeners());
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_camera, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_camera, menu);
-        return true;
-    }
+	private void initUI(){
+		glSurfaceView = (CameraGLSurfaceView)findViewById(R.id.camera_textureview);
+		shutterBtn = (ImageButton)findViewById(R.id.btn_shutter);
+	}
+	private void initViewParams(){
+		LayoutParams params = glSurfaceView.getLayoutParams();
+		Point p = DisplayUtil.getScreenMetrics(this);
+		params.width = p.x;
+		params.height = p.y;
+		previewRate = DisplayUtil.getScreenRate(this); //默认全屏的比例预览
+		glSurfaceView.setLayoutParams(params);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		//手动设置拍照ImageButton的大小为120dip×120dip,原图片大小是64×64
+		LayoutParams p2 = shutterBtn.getLayoutParams();
+		p2.width = DisplayUtil.dip2px(this, 80);
+		p2.height = DisplayUtil.dip2px(this, 80);
+		shutterBtn.setLayoutParams(p2);	
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+	}
 
-        return super.onOptionsItemSelected(item);
-    }
+	private class BtnListeners implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch(v.getId()){
+			case R.id.btn_shutter:
+				CameraInterface.getInstance().doTakePicture();
+				break;
+			default:break;
+			}
+		}
+
+	}
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		glSurfaceView.bringToFront();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		glSurfaceView.onPause();
+	}
+
+
 }
